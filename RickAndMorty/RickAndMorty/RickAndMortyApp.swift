aec0ba0a796e3897f5c33544fe132855e3e4da5d
rimKtari <ktari.rima1@gmail.com>
2024-03-09 15:03:08 +0100

    create project

---
+++ RickAndMorty/RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by RIMA on 9/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
